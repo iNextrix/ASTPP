88308485dbcdf628a0f23cfb3a95b0007be2c82f
Samir <samir.doshi@inextrix.com>
2016-07-27 17:45:55 +0530

    [IMP] Updated installation script, corrected url path in json_cdr of fs, Add update.sh to update 3.0 with latest changes

--- install.sh
+++ install.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-###########################################################################
+###############################################################################
 # ASTPP - Open Source VoIP Billing Solution
-# Copyright (C) 2016, iNextrix Technologies Pvt. Ltd. (http://www.inextrix.com)
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# Copyright (C) 2016 iNextrix Technologies Pvt. Ltd.
+# ASTPP Version 3.0
+# License https://www.gnu.org/licenses/agpl-3.0.html
 #
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details..
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>
-#
-############################################################################
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
 
 #################################
 ##########  variables ###########
---
+++ update.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+###############################################################################
+# ASTPP - Open Source VoIP Billing Solution
+#
+# Copyright (C) 2016 iNextrix Technologies Pvt. Ltd.
+# ASTPP Version 3.0
+# License https://www.gnu.org/licenses/agpl-3.0.html
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+NOW=$(date +"%m-%d-%Y-%H-%M-%S")
+BACKUPDIR="/opt/ASTPP3_Backup"
+TEMPDIR="/opt/ASTPP3_patch"
+if [ -d "${BACKUPDIR}" ]
+then
+        echo "Backup directory  exists!"
+else
+        echo "Backup directory not found!!!"."Creating Backup location . . . . ."
+        mkdir ${BACKUPDIR}
+fi
+echo "date = "${NOW}
+cd ${BACKUPDIR}
+echo "Creating Backups files . . . . ."
+tar czf ASTPP3_html_astpp_${NOW}.tar.gz /var/www/html/astpp
+tar czf ASTPP3_html_fs_${NOW}.tar.gz /var/www/html/fs
+tar czf ASTPP3_local_fsscripts_${NOW}.tar.gz /usr/local/freeswitch/scripts
+echo "Backups files created at location "${BACKUPDIR}
+
+mkdir ${TEMPDIR}
+cd ${TEMPDIR}
+git clone https://github.com/iNextrix/ASTPP
+cd ASTPP
+echo "Updating your current source with latest one . . . . . "
+rm -rf /var/www/html/astpp/*
+cp -rf web_interface/astpp/* /var/www/html/astpp/.
+chown -Rf root.root /var/www/html/astpp
+chmod -Rf 777 /var/www/html/astpp
+rm -rf /var/www/html/fs/*
+cp -rf freeswitch/fs/* /var/www/html/fs/.
+chown -Rf root.root /var/www/html/fs
+rm -rf /usr/local/freeswitch/scripts/*
+cp -rf freeswitch/scripts/* /usr/local/freeswitch/scripts/.
+echo "Update successfully Completed !!!!!!"
+
+rm -rf ${TEMPDIR}
