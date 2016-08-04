3702ba8411c89be0fa3cc15c0a55c6a71ecc7205
dpa-astpp <deep.patel@inextrix.com>
2016-08-04 18:51:40 +0530

    [FIX]Add two fields in Opensips and changes in sql files

--- database/opensips_chages.sql
+++ database/opensips_chages.sql
@@ -3,4 +3,6 @@ ALTER table subscriber Add pricelist_id int(11) after `accountcode`;
 
 ALTER TABLE `subscriber` ADD `status` TINYINT( 1 ) NOT NULL DEFAULT '0' COMMENT '0:active,1:inactive',
 ADD `creation_date` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
-ADD `last_modified_date` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00'
+ADD `last_modified_date` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00';
+
+ALTER TABLE `subscriber` ADD `reseller_id` INT( 4 ) NOT NULL ;
