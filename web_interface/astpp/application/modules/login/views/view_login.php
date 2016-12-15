924639b10122c30dbec1d9f29cbdd3a235230c01
Deep <deep.patel@inextrix.com>
2016-12-15 11:07:40 +0530

    [FIX]Issues solving of ISSUE-7554

--- web_interface/astpp/application/modules/login/views/view_login.php
+++ web_interface/astpp/application/modules/login/views/view_login.php
@@ -77,13 +77,10 @@
 							}
 
 							if ($this->session->userdata('userlevel_logintype') != '0') {?>
-								<a class="col-md-12" style="padding:0px 0px 10px 0px" href="<?php echo base_url(); ?>">
-									<img style="height: 44px;" id="logo" alt="dashboard" src="<?php echo base_url(); ?>upload/<?php echo$logo;?>">
+									<img style="height:53px;widht:216px;" id="logo" alt="login" src="<?php echo base_url(); ?>upload/<?php echo$logo;?>">
 							<? } else {?> 
-									<a class="col-md-12" style="padding:0px 0px 20px 0px" href="<?php echo base_url(); ?>">
-									<img style="height: 44px;" id="logo" title='ASTPP - Open Source Voip Billing Solution' alt='ASTPP - Open Source Voip Billing Solution' src="<?php echo base_url(); ?>upload/<?php echo$logo;?>">
+									<img style="height:53px;widht:216px;" id="logo" title='ASTPP - Open Source Voip Billing Solution' alt='login' src="<?php echo base_url(); ?>upload/<?php echo$logo;?>">
 							<? }?>
-								</a>
                             	<div class="clear"></div>
                             
                             </h2>
