495b61170d56edbb8c768c26bad209a53f07ee14
Samir Doshi <samir.doshi@inextrix.com>
2017-02-28 19:14:00 +0530

    [FIX] Resolved footer issues

--- web_interface/astpp/application/views/footer.php
+++ web_interface/astpp/application/views/footer.php
@@ -10,7 +10,7 @@
 	$this->db->select('*');	
 	$this->db->where('domain',$_SERVER['HTTP_HOST']);
 	$this->db->or_where('domain','http://'.$_SERVER['HTTP_HOST']);
-	$this->db->or_where('domain','https://'.$_SERVER['HTTP_HOST'])
+	$this->db->or_where('domain','https://'.$_SERVER['HTTP_HOST']);
 	$result=$this->db->get('invoice_conf');
 	if($result->num_rows() > 0){
 		$result=$result->result_array();
