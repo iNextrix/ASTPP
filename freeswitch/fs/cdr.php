1904a7689a9941320267a49032ed14b98c8877c7
Samir Doshi <samir.doshi@inextrix.com>
2017-09-05 18:30:35 +0530

    Version 3.5

--- freeswitch/fs/cdr.php
+++ freeswitch/fs/cdr.php
@@ -60,7 +60,11 @@ $logger = new logger ( $lib );
 if (isset ( $_SERVER ["CONTENT_TYPE"] ) && $_SERVER ["CONTENT_TYPE"] == "application/json") {
 	
 	$db->run ( "SET NAMES utf8" );
-	$data = json_decode ( file_get_contents ( "php://input" ), true );
+	//$data = json_decode ( file_get_contents ( "php://input" ), true );
+	$data = file_get_contents("php://input");
+        $data = utf8_encode($data);
+	$data = json_decode($data,true);
+
 	// error_log(print_r($data,true));
 	$logger->log ( print_r ( $data, true ) );
 	// process_cdr($data,$db,$logger,$decimal_points);
