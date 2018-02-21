c7d1d7c5a169396d13ff8dcaf09fa7566a98422f
Samir Doshi <samir.doshi@inextrix.com>
2018-02-21 15:02:44 +0530

    Removed unnecessary files

--- web_interface/astpp/application/config/cron.php
+++ web_interface/astpp/application/config/cron.php
@@ -15,13 +15,11 @@ $config ['SERVER_NAME'] = $astpp_config ['base_url']; // Your web site url
 $config ['CRON_TIME_LIMIT'] = 0; // 0 = no time limit
 $config ['argv'] = array (
 		"LowBalance" => "lowbalance/low_balance",
-		"CurrencyUpdate" => "currencyupdate/update_currency",
-								/*ASTPP_invoice_changes_05_05_start*/
-								"GenerateInvoice" => "ProcessInvoice/GenerateInvoice",
+		"CurrencyUpdate" => "currencyupdate/update_currency",								
+		"GenerateInvoice" => "ProcessInvoice/GenerateInvoice",
 		"UpdateBalance" => "ProcessCharges/GetUpdateBalance",
 		"ProcessDailyCharges" => "ProcessCharges/ProcessDailyCharges",
-		"BillAccountCharges" => "ProcessCharges/BillAccountCharges",
-		// end
+		"BillAccountCharges" => "ProcessCharges/BillAccountCharges",		
 		"FeedBack" => "feedback/customer_feedback_result/TRUE",
 		"BroadcastEmail" => "broadcastemail/broadcast_email" 
 );
