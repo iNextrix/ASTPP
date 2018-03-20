734f766d6b91bc2c6548857deca20a7c896a3224
Samir Doshi <samir.doshi@inextrix.com>
2018-03-20 15:18:41 +0530

    [DOC] Updated configuration

--- doc/conf.py
+++ doc/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ASTPP'
-copyright = u'2017 iNextrix Technologies Pvt. Ltd'
+copyright = u'2018 iNextrix Technologies Pvt. Ltd'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
