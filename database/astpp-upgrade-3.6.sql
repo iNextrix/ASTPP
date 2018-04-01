30d56719588baa3cde07ccb724e9076c6d6bf471
Rob Thomas <xrobau@gmail.com>
2018-04-01 22:15:26 +0000

    More SQL Fixes.

--- database/astpp-upgrade-3.6.sql
+++ database/astpp-upgrade-3.6.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS `q850code`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `q850code` (
-  `cause` varchar(70) DEFAULT NULL,
+  `cause` varchar(70) NOT NULL PRIMARY KEY,
   `code` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
