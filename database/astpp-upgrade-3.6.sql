c2ae7af9f0b63784f3779f54800fd4afe38f1018
Rob Thomas <xrobau@gmail.com>
2018-04-04 07:43:46 +0000

    More database fixes

--- database/astpp-upgrade-3.6.sql
+++ database/astpp-upgrade-3.6.sql
@@ -97,5 +97,6 @@ ALTER TABLE `cdrs` MODIFY `answer_stamp` datetime NULL DEFAULT '1980-01-01 00:00
 ALTER TABLE `cdrs` MODIFY `bridge_stamp` datetime NULL DEFAULT '1980-01-01 00:00:00';
 ALTER TABLE `cdrs` MODIFY `progress_stamp` datetime NULL DEFAULT '1980-01-01 00:00:00';
 ALTER TABLE `cdrs` MODIFY `progress_media_stamp` datetime NULL DEFAULT '1980-01-01 00:00:00';
+ALTER TABLE `cdrs` MODIFY `callstart` datetime NULL DEFAULT '1980-01-01 00:00:00';
 
 
