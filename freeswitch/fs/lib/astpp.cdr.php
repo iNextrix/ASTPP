fcded47a42f22cf12598a7693192a8818bf466b3
Samir Doshi <samir.doshi@inextrix.com>
2018-04-05 18:14:34 +0530

    [IMP] Set default per second billing if no initial increment and increment defained in origination rate and rate group

--- freeswitch/fs/lib/astpp.cdr.php
+++ freeswitch/fs/lib/astpp.cdr.php
@@ -515,6 +515,7 @@ function calc_cost($dataVariable, $rates, $logger, $decimal_points) {
 	}
 	if ($duration > 0 && isset($rates['INC'])) {
 		$rates ['INC'] = ($rates ['INC'] == 0) ? 1 : $rates ['INC'];
+		$rates ['INITIALBLOCK'] = ($rates ['INITIALBLOCK'] == 0) ? 1 : $rates ['INITIALBLOCK'];
 		$call_cost = $rates ['CONNECTIONCOST'];
 		$call_cost += ($rates ['INITIALBLOCK'] * $rates ['COST']) / 60;
 		$billseconds = $duration - $rates ['INITIALBLOCK'];
