c841694120c634841fabb3a4e572af96d038dd35
ankitdoshi <4167853+ankitdoshi@users.noreply.github.com>
2018-04-06 18:46:03 +0530

    [FIX] https domain not working
    Web portal working on https and if company profile have not proper domain then it was stop working.

--- web_interface/astpp/application/hooks/router.php
+++ web_interface/astpp/application/hooks/router.php
@@ -2,26 +2,31 @@
 if(!defined( 'BASEPATH' ) ) exit( 'No direct script access allowed' );
 
 class Router {
-      public static $_domain	= '';
+      public static $_domain    = '';
       public function route() {
-	if(isset($_SERVER["HTTP_HOST"]) && $_SERVER["HTTP_HOST"] != ''){
-	  require_once(BASEPATH.'database/DB.php');
-	  $db = DB(); // getting hold of a DAO instance
-	  $db->select("domain");
- 	  $db->where(array("domain"=>$_SERVER["HTTP_HOST"]));
-	  $res = $db->get("invoice_conf");
-	  $domain = $res->result(); 
-	  if(!empty($domain) && $domain[0]->domain != ""){
-	    self::$_domain = "http://".$domain[0]->domain."/";
-	  }
-	}
+        if(isset($_SERVER["HTTP_HOST"]) && $_SERVER["HTTP_HOST"] != ''){
+          require_once(BASEPATH.'database/DB.php');
+          $db = DB(); // getting hold of a DAO instance
+          $db->select("domain");
+          $db->where(array("domain"=>$_SERVER["HTTP_HOST"]));
+          $res = $db->get("invoice_conf");
+          $domain = $res->result();
+          if(!empty($domain) && $domain[0]->domain != ""){
+            if($_SERVER['HTTPS'] == "on"){
+                self::$_domain = "https://".$domain[0]->domain."/";
+            }else{
+                self::$_domain = "http://".$domain[0]->domain."/";
+            }
+          }
+        }
       }
 
       public function config() {
-	  if(self::$_domain != ""){
-	    $RTR = load_class('Router','core');
-	    $RTR->config->config['base_url'] = self::$_domain;
-	    return true;
-	  }
+          if(self::$_domain != ""){
+            $RTR = load_class('Router','core');
+            $RTR->config->config['base_url'] = self::$_domain;
+            return true;
+          }
       }
 }
+?>
