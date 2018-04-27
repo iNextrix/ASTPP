cb782df41f73a9045aaf35ae6ca7c593f9ba5a4d
Rob Thomas <xrobau@gmail.com>
2018-04-27 23:23:51 +0000

    Fix 358e347932ebdd322098fb471aaf4f1ac70b0fca
    * Tax numbers should not be mandatory.

--- database/astpp-3.0.sql
+++ database/astpp-3.0.sql
@@ -599,7 +599,7 @@ CREATE TABLE `invoice_conf` (
   `invoice_notification` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0:enable,1:disable',
   `interval` varchar(11) NOT NULL,
   `notify_before_day` int(11) NOT NULL DEFAULT '1',
-  `invoice_taxes_number` varchar(100) NOT NULL DEFAULT 'ABN 12 345 678 901',
+  `invoice_taxes_number` varchar(100) NOT NULL DEFAULT '',
   `domain` varchar(100) NOT NULL,
   `website_title` varchar(100) NOT NULL,
   `website_footer` varchar(100) NOT NULL,
