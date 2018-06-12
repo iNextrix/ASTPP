06e4814c64b07c8be6db208fe5ad2cf856a8801a
Devang Nathwani <devang.nathwani31589@gmail.com>
2018-06-12 17:54:50 +0530

    Modifying IP validation process

--- web_interface/astpp/system/core/Input.php
+++ web_interface/astpp/system/core/Input.php
@@ -371,30 +371,11 @@ class CI_Input {
 	*/
 	function valid_ip($ip)
 	{
-		$ip_segments = explode('.', $ip);
-
-		// Always 4 segments needed
-		if (count($ip_segments) != 4)
-		{
-			return FALSE;
-		}
-		// IP can not start with 0
-		if ($ip_segments[0][0] == '0')
-		{
-			return FALSE;
-		}
-		// Check each segment
-		foreach ($ip_segments as $segment)
-		{
-			// IP segments must be digits and can not be
-			// longer than 3 digits or greater then 255
-			if ($segment == '' OR preg_match("/[^0-9]/", $segment) OR $segment > 255 OR strlen($segment) > 3)
-			{
-				return FALSE;
-			}
-		}
-
-		return TRUE;
+		if (filter_var($ip, FILTER_VALIDATE_IP) || preg_match("/^(?!\-)(?:[a-zA-Z\d\-]{0,62}[a-zA-Z\d]\.){1,126}(?!\d+)[a-zA-Z\d]{1,63}$/", $ip)) {
+                        return TRUE;
+                } else {
+                        return FALSE;
+                }
 	}
 
 	// --------------------------------------------------------------------
