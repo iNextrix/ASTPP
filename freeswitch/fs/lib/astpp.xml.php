2cb29dfa1df9baba3172cb5d64b39c7f39f53ecf
Devang Nathwani <devang.nathwani31589@gmail.com>
2018-06-12 18:19:29 +0530

    Modifying dynamic XML file generation process -ACL

--- freeswitch/fs/lib/astpp.xml.php
+++ freeswitch/fs/lib/astpp.xml.php
@@ -51,6 +51,12 @@ function load_acl($logger, $db, $config) {
 	$logger->log ( $res_acl );
 	
 	foreach ( $res_acl as $res_acl_key => $res_acl_value ) {
+		if(preg_match("/[a-zA-Z\-]/i", $res_acl_value ['ip'])){
+                        $ips = gethostbynamel($res_acl_value ['ip']);
+                        foreach ($ips as $ip => $value){
+                                $res_acl_value ['ip'] = $value . "/32";
+                        }
+                }
 		$xml .= "       <node type=\"allow\" cidr=\"" . $res_acl_value ['ip'] . "\"/>\n";
 	}
 	
@@ -66,7 +72,13 @@ function load_acl($logger, $db, $config) {
 		foreach ( $sp_gw_settings as $sp_gw_settings_key => $sp_gw_settings_value ) {
 			if ($sp_gw_settings_value != "" && $sp_gw_settings_key == "proxy") {
 				$tmp_ip_arr = explode ( ":", $sp_gw_settings_value );
-				if (! filter_var ( $tmp_ip_arr [0], FILTER_VALIDATE_IP ) === false) {
+				if (filter_var($tmp_ip_arr [0], FILTER_VALIDATE_IP) || preg_match("/^(?!\-)(?:[a-zA-Z\d\-]{0,62}[a-zA-Z\d]\.){1,126}(?!\d+)[a-zA-Z\d]{1,63}$/", $tmp_ip_arr [0])) {
+                                        if(preg_match("/[a-zA-Z\-]/i", $tmp_ip_arr [0])){
+                                                $ips = gethostbynamel($tmp_ip_arr [0]);
+                                                foreach ($ips as $ip => $value){
+                                                        $tmp_ip_arr [0] = $value;
+                                                }
+                                        }
 					$xml .= "   <node type=\"allow\" cidr=\"" . $tmp_ip_arr [0] . "/32\"/>\n";
 				}
 			}
@@ -75,21 +87,28 @@ function load_acl($logger, $db, $config) {
 	
 	// For opensips
 	if ($config ['opensips'] == '0') {
-		$xml .= "<node type=\"allow\" cidr=\"" . $config ['opensips_domain'] . "/32\"/>\n";
+		if(preg_match("/[a-zA-Z\-]/i", $config ['opensips_domain'])){
+                        $ips = gethostbynamel($config ['opensips_domain']);
+                        foreach ($ips as $ip => $value){ 
+                                $config ['opensips_domain'] = $value;
+                        }
+                }
+                $xml .= "<node type=\"allow\" cidr=\"" . $config ['opensips_domain'] . "/32\"/>\n";
+                $xml .= "       </list>\n";
+		// For loopback
+                $xml .= "<list name=\"loopback.auto\" default=\"allow\">\n";
+                $xml .= "<node type=\"allow\" cidr=\"" . $config ['opensips_domain'] . "/32\"/>\n";
+                $xml .= "</list>\n";
+		// For event handing
+                $xml .= "<list name=\"event\" default=\"deny\">\n";
+                $xml .= "<node type=\"allow\" cidr=\"" . $config ['opensips_domain'] . "/32\"/>\n";
 	}
+	else{
+                $xml .= "       </list>\n";
+		// For event handing
+                $xml .= "<list name=\"event\" default=\"deny\">\n";
+        }
 	
-	$xml .= "       </list>\n";
-	
-	// For loopback
-	if ($config ['opensips'] == '0') {
-		$xml .= "<list name=\"loopback.auto\" default=\"allow\">\n";
-		$xml .= "<node type=\"allow\" cidr=\"" . $config ['opensips_domain'] . "/32\"/>\n";
-		$xml .= "</list>\n";
-	}
-	
-	// For event handing
-	$xml .= "<list name=\"event\" default=\"deny\">\n";
-	$xml .= ($config ['opensips'] == "0") ? "<node type=\"allow\" cidr=\"" . $config ['opensips_domain'] . "/32\"/>\n" : "\n";
 	$xml .= "<node type=\"allow\" cidr=\"127.0.0.0/8\"/>\n";
 	$xml .= "</list>\n";
 	$xml .= "           </network-lists>\n";
