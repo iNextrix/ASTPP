e1c60876f9fb16f86f101182dd197c5b29e59e86
PowerPBX <admin@powerpbx.org>
2021-10-12 12:37:26 -0700

    Fix Exploit EDB-ID: 47889 https://www.exploit-db.com/exploits/47889
    Found more and fixed xss_clean

--- web_interface/astpp/application/modules/invoices/libraries/invoices_form.php
+++ web_interface/astpp/application/modules/invoices/libraries/invoices_form.php
@@ -925,7 +925,7 @@ class invoices_form extends common
                     'size' => '20',
                     'class' => "text field medium"
                 ),
-                'required',
+                'required|xss_clean',
                 'tOOL TIP',
                 'Please Enter account number'
             ),
@@ -1217,7 +1217,7 @@ class invoices_form extends common
                     'maxlength' => '100',
                     'class' => "text field medium"
                 ),
-                '',
+                'xss_clean',
                 'tOOL TIP',
                 ''
             ),
