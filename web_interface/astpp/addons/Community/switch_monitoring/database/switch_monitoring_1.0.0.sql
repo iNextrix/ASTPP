921c4f8321efcc5f6bf353fd5352ea0ec20ccfdb
ashish.inextrix <ashish.gohil@inextrix.in>
2022-01-06 18:41:20 +0530

    [IMP]Make switch monitoring addon compatible with community edition with license checking

--- web_interface/astpp/addons/Community/switch_monitoring/database/switch_monitoring_1.0.0.sql
+++ web_interface/astpp/addons/Community/switch_monitoring/database/switch_monitoring_1.0.0.sql
@@ -1,7 +1,7 @@
 INSERT INTO `menu_modules` (`id`, `menu_label`, `module_name`, `module_url`, `menu_title`, `menu_image`, `menu_subtitle`, `priority`)
 VALUES (NULL, 'Registered SIP Devices', 'sip', 'switch_monitoring/sip_devices/', 'Switch', 'Live-Report.png', '0', 100.6);
 UPDATE `userlevels` SET `module_permissions` = concat( `module_permissions`, ',', (  SELECT max( `id` ) FROM `menu_modules` ) ) WHERE `userlevelid` = -1;
-UPDATE `userlevels` SET `module_permissions` = concat( `module_permissions`, ',', (  SELECT max( `id` ) FROM `menu_modules` ) ) WHERE `userlevelid` = 1;
+-- UPDATE `userlevels` SET `module_permissions` = concat( `module_permissions`, ',', (  SELECT max( `id` ) FROM `menu_modules` ) ) WHERE `userlevelid` = 1;
 
 INSERT INTO `menu_modules` (`id`, `menu_label`, `module_name`, `module_url`, `menu_title`, `menu_image`, `menu_subtitle`, `priority`) VALUES
 (NULL, 'Registered Gateways', 'gateways', 'switch_monitoring/gateways/', 'Switch', 'TrunkStats.png', '0', 100.7);
--- web_interface/astpp/addons/Community/switch_monitoring/database/switch_monitoring_2.0.0.sql
+++ web_interface/astpp/addons/Community/switch_monitoring/database/switch_monitoring_2.0.0.sql
@@ -1,7 +1,7 @@
 -- sandip add roles and permission in admin 2-4-20
 
 UPDATE `userlevels` SET `module_permissions` = concat( `module_permissions`, ',', (  SELECT max( `id` ) FROM `menu_modules` where `module_url` = "switch_monitoring/sip_devices/" ) ) WHERE `userlevelid` = 1;
-INSERT INTO `roles_and_permission` (`id`, `login_type`, `permission_type`, `menu_name`, `module_name`, `sub_module_name`, `module_url`, `display_name`, `permissions`, `status`, `creation_date`, `priority`) VALUES (NULL, '1', '0', 'switch', 'switch_monitoring', '', 'sip_devices', 'Registered SIP Devices', '[\"main\",\"list\"]', '0', '2019-01-25 09:01:05', '6');
+INSERT INTO `roles_and_permission` (`id`, `login_type`, `permission_type`, `menu_name`, `module_name`, `sub_module_name`, `module_url`, `display_name`, `permissions`, `status`, `creation_date`, `priority`) VALUES (NULL, '1', '0', 'switch', 'switch_monitoring', '', 'sip_devices', 'Registered SIP Device', '[\"main\",\"list\"]', '0', '2019-01-25 09:01:05', '6.34');
 
 -- sandip add roles and permission in admin 2-4-20
 -- INSERT INTO `menu_modules` (`id`, `menu_label`, `module_name`, `module_url`, `menu_title`, `menu_image`, `menu_subtitle`, `priority`)
