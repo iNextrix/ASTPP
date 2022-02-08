ac61ad65329836f33a82aa3c879fe64753cd0679
AshishGohil840 <73466505+AshishGohil840@users.noreply.github.com>
2022-02-08 13:29:11 +0530

    Update switch_monitoring_1.0.1.sql

--- web_interface/astpp/addons/Community/switch_monitoring/database/switch_monitoring_1.0.1.sql
+++ web_interface/astpp/addons/Community/switch_monitoring/database/switch_monitoring_1.0.1.sql
@@ -1,7 +1,7 @@
 -- sandip add roles and permission in admin 2-4-20
 
 UPDATE `userlevels` SET `module_permissions` = concat( `module_permissions`, ',', (  SELECT max( `id` ) FROM `menu_modules` where `module_url` = "switch_monitoring/sip_devices/" ) ) WHERE `userlevelid` = 2;
-INSERT INTO `roles_and_permission` (`id`, `login_type`, `permission_type`, `menu_name`, `module_name`, `sub_module_name`, `module_url`, `display_name`, `permissions`, `status`, `creation_date`, `priority`) VALUES (NULL, '0', '0', 'switch', 'switch_monitoring', '', 'sip_devices', 'Registered SIP Device', '[\"main\",\"list\"]', '0', '2019-01-25 09:01:05', '6.30');
+INSERT INTO `roles_and_permission` (`id`, `login_type`, `permission_type`, `menu_name`, `module_name`, `sub_module_name`, `module_url`, `display_name`, `permissions`, `status`, `creation_date`, `priority`) VALUES (NULL, '0', '0', 'switch', 'switch_monitoring', '', 'sip_devices', 'Register SIP Device', '[\"main\",\"list\"]', '0', '2019-01-25 09:01:05', '6.30');
 
 UPDATE `userlevels` SET `module_permissions` = concat( `module_permissions`, ',', (  SELECT max( `id` ) FROM `menu_modules` where `module_url` = "switch_monitoring/gateways/" ) ) WHERE `userlevelid` = 2;
 INSERT INTO `roles_and_permission` (`id`, `login_type`, `permission_type`, `menu_name`, `module_name`, `sub_module_name`, `module_url`, `display_name`, `permissions`, `status`, `creation_date`, `priority`) VALUES (NULL, '0', '0', 'switch', 'switch_monitoring', '', 'gateways', 'Registered Gateways', '[\"main\",\"list\"]', '0', '2019-01-25 09:01:05', '6.31');
